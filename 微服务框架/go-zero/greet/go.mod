f49775a81fb306f738855a84e1387a046ff98a03
gocloudcoder <nstar@gocloudcoder.com>
2021-05-21 13:34:13 +0800

    add go-zero

---
+++ 微服务框架/go-zero/greet/go.mod
@@ -0,0 +1,5 @@
+module greet
+
+go 1.16
+
+require github.com/tal-tech/go-zero v1.1.7
---
+++ 微服务框架/go-zero/greet/internal/handler/greethandler.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"net/http"
+
+	"greet/internal/logic"
+	"greet/internal/svc"
+	"greet/internal/types"
+
+	"github.com/tal-tech/go-zero/rest/httpx"
+)
+
+func GreetHandler(ctx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.Request
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
+		l := logic.NewGreetLogic(r.Context(), ctx)
+		resp, err := l.Greet(req)
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.OkJson(w, resp)
+		}
+	}
+}

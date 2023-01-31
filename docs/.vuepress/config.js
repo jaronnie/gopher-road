6404035d245c97c85901bf1f00cfb5fa2ee4c6fc
jaronnie <jaron@jaronnie.com>
2023-01-31 14:47:17 +0800

    feat(road): update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
         { rel: 'icon', href: 'favicon-gopher-road.ico' }
     ]
 ],
-  base: '/',
+  base: '/gopher-road/',
   locales: {
     // 键名是该语言所属的子路径
     // 作为特例，默认语言可以使用 '/' 作为其路径。

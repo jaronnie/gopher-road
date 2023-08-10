6a63d1c4ed0052a74153d270e95c11953261c632
jaronnie <jaron@jaronnie.com>
2023-08-10 11:21:50 +0800

    feat(road): fix config.js format

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -29,33 +29,33 @@ module.exports = {
     locales: {
       '/': {
        // 多语言下拉菜单的标题
-       selectText: '选择语言',
+      selectText: '选择语言',
        // 该语言在下拉菜单中的标签
-       label: '简体中文',
+      label: '简体中文',
        // Service Worker 的配置
-       serviceWorker: {
-         updatePopup: {
-           message: "发现新内容可用.",
-           buttonText: "刷新"
-         }
-       },
+      serviceWorker: {
+        updatePopup: {
+          message: "发现新内容可用.",
+          buttonText: "刷新"
+        }
+      },
        // 当前 locale 的 algolia docsearch 选项
-       algolia: {},
-       nav: [
-         { text: 'golang', link: '/golang/', ariaLabel: '指南' },
-         { text: 'linux', link: '/linux/', ariaLabel: '指南' },
-         { text: '算法与数据结构', link: '/algorithms/', ariaLabel: '指南' },
-         { text: '代码管理', link: '/codeManage/', ariaLabel: '指南' },
-         { text: '中间件', link: '/middleware/', ariaLabel: '指南' },
-         { text: 'docker', link: '/docker/', ariaLabel: '指南' },
-         { text: 'kubernetes', link: '/kubernetes/', ariaLabel: '指南' },
-         { text: '项目实战', link: '/project/', ariaLabel: '指南' },
-         { text: '推荐阅读', link: '/recommendation/', ariaLabel: '指南' },
-         { text: 'Github', link: 'https://github.com/jaronnie/gopher-road' },
-         { text: 'Blog', link: 'https://blog.jaronnie.com' }
-       ],
-       sidebar: {
-         '/golang/': [
+      algolia: {},
+      nav: [
+        { text: 'golang', link: '/golang/', ariaLabel: '指南' },
+        { text: 'linux', link: '/linux/', ariaLabel: '指南' },
+        { text: '算法与数据结构', link: '/algorithms/', ariaLabel: '指南' },
+        { text: '代码管理', link: '/codeManage/', ariaLabel: '指南' },
+        { text: '中间件', link: '/middleware/', ariaLabel: '指南' },
+        { text: 'docker', link: '/docker/', ariaLabel: '指南' },
+        { text: 'kubernetes', link: '/kubernetes/', ariaLabel: '指南' },
+        { text: '项目实战', link: '/project/', ariaLabel: '指南' },
+        { text: '推荐阅读', link: '/recommendation/', ariaLabel: '指南' },
+        { text: 'Github', link: 'https://github.com/jaronnie/gopher-road' },
+        { text: 'Blog', link: 'https://blog.jaronnie.com' }
+      ],
+      sidebar: {
+        '/golang/': [
           {
             title: '标准库',
             collapsable: true,
@@ -91,7 +91,7 @@ module.exports = {
             children: [
               { title: 'http', path: '/golang/3-web-frame/http' },
               { title: 'implement-http', path: '/golang/3-web-frame/implement-http' },
-			  { title: 'http-unix-socket-tcp', path: '/golang/3-web-frame/http-unix-socket-tcp'},
+              { title: 'http-unix-socket-tcp', path: '/golang/3-web-frame/http-unix-socket-tcp'},
               { title: 'gin', path: '/golang/3-web-frame/gin' },
             ]
           },
@@ -106,29 +106,29 @@ module.exports = {
               { title: 'golang pprof', path: '/golang/5-others/golang-pprof' },
             ]
           },
-         ],
-         '/linux/': [
-           'why-need-linux',
-           'du',
-           'silversearcher-ag',
-           'grep-sed-awk',
-           'compress-decompress',
-           'copy-compare-cut',
-           'ssh-free-password',
-           'linux-firewall'
-         ],
-         '/algorithms/': [
-           'kahan-summation',
-         ],
-         '/codeManage/': [
-           'gomodule',
-           'git-workflow',
-           'git-use',
-           'jenkins-go',
-           'git-lfs',
-           'github-action',
-         ],
-         '/middleware/': [
+        ],
+        '/linux/': [
+          'why-need-linux',
+          'du',
+          'silversearcher-ag',
+          'grep-sed-awk',
+          'compress-decompress',
+          'copy-compare-cut',
+          'ssh-free-password',
+          'linux-firewall'
+        ],
+        '/algorithms/': [
+          'kahan-summation',
+        ],
+        '/codeManage/': [
+          'gomodule',
+          'git-workflow',
+          'git-use',
+          'jenkins-go',
+          'git-lfs',
+          'github-action',
+        ],
+        '/middleware/': [
           {
             title: 'mongodb',
             collapsable: true,
@@ -143,15 +143,15 @@ module.exports = {
               { title: 'rabbitmq 入门使用', path: '/middleware/rabbitmq/start' },
             ]
           },
-         {
-             title: 'prometheus',
-             collapsable: true,
-             children: [
-                 { title: '将推送数据到 pushgateway', path: '/middleware/prometheus/push-data-pushgateway' },
-             ]
-         },
+        {
+            title: 'prometheus',
+            collapsable: true,
+            children: [
+                { title: '将推送数据到 pushgateway', path: '/middleware/prometheus/push-data-pushgateway' },
+            ]
+        },
         ],
-         '/docker/': [
+        '/docker/': [
           'what-is-docker',
           'docker-install',
           'docker-create-run-use-container',
@@ -160,19 +160,19 @@ module.exports = {
           'docker-practice-ubuntu-ssh',
           'docker-practice-mysql',
           'docker-manage-image-container.md'
-         ],
-         '/kubernetes/': [
+        ],
+        '/kubernetes/': [
           'k8s-image-pull-policy'
-         ],
-         '/project/': [
-         ],
-         '/recommendation/': [
+        ],
+        '/project/': [
+        ],
+        '/recommendation/': [
           'go-code-guide',
           'go-books-read',
           'go-leetcode',
           'auto-blog',
-         ]
-       }
+        ]
+      }
       },
     }
   },
